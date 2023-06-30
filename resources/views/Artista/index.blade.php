922d5d136e6e4c103c2f2999afdfe027d258d61c
Lolregsito <amigocordovanelson@gmail.com>
2023-06-29 20:07:20 -0400

    el server no funciona

--- resources/views/Artista/index.blade.php
+++ resources/views/Artista/index.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/d24bea0f94.js" crossorigin="anonymous"></script>
-    <title>Perfiles</title>
+    <title>Perfil</title>
 </head>
 <body>
      <!-- NAVBAR -->
@@ -46,6 +46,7 @@
           </div>
         </div>
       </nav>
+
 
 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
